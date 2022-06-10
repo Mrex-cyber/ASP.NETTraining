50c8c3225ba5f110d194c14d47de15b49aebfe63
Valentyn <54396051+Mrex-cyber@users.noreply.github.com>
2022-06-10 20:26:07 +0300

    Services
    I made own service

--- MetanitASPLearn/Program.cs
+++ MetanitASPLearn/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder();
+builder.Services.AddTransient<ITimeService, GetShortTimeService>();
 var app = builder.Build();
 
-app.Environment.EnvironmentName = "Test";   // измен€ем название среды на Test
 
-if (app.Environment.IsEnvironment("Test")) // ≈сли проект в состо€нии "Test"
+app.Environment.EnvironmentName = "Test";   
+
+app.Run(async context =>
+{
+    var timeService = app.Services.GetService<ITimeService>();
+    await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
+});
+
+if (app.Environment.IsEnvironment("Test"))
 {
     app.Run(async (context) => await context.Response.WriteAsync("In Test Stage"));
 }
@@ -13,3 +21,17 @@ else
 }
 
 app.Run();
+
+interface ITimeService
+{
+    string GetTime();
+}
+
+class GetShortTimeService : ITimeService
+{
+    public string GetTime() => DateTime.Now.ToShortTimeString();
+}
+class GetlongTimeService : ITimeService
+{
+    public string GetTime() => DateTime.Now.ToLongTimeString();
+}
